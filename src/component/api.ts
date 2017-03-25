51961b770de4281ff636cbccc0208ce79426f750
twp0217 <tu.wenpan@qq.com>
2017-03-25 16:42:59 +0800

    演示分支清空

--- src/component/api.ts
+++
@@ -1,6 +0,0 @@
-export interface Ng2Ueditor {
-    getContent(): string;
-    getPlainTxt(): string;
-    getContentTxt(): string;
-    hasContents(): boolean;
-}
--- src/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './component/api';
-export * from './component/ng2-ueditor.component';
-export * from './component/ng2-ueditor.module';
